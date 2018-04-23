ac60ae5dc604c6371df1658cea4b43e878657b20
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DataSourceWizardCustomization/Form1.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardCommon.DataSourceWizard;
+using DevExpress.DashboardWin;
+using DevExpress.DashboardWin.DataSourceWizard;
+using DevExpress.DashboardWin.Native;
+using DevExpress.DataAccess.UI.Wizard;
+
+namespace DataSourceWizardCustomization {
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm {
+        public Form1() {
+            InitializeComponent();
+            dashboardDesigner1.CreateRibbon();
+            dashboardDesigner1.DataSourceWizardCustomizationService = new DataSourceWizardCustomizationService();
+            dashboardDesigner1.ShowDataSourceWizard();
+        }
+    }
+
+    public class DataSourceWizardCustomizationService : IDataSourceWizardCustomizationService {
+        public void CustomizeDataSourceWizard(IWizardCustomization<DashboardDataSourceModel> customization) {
+            customization.RegisterPageView<IConfigureOlapParametersPageView, CustomConfigureOlapParametersPageView>();
+        }
+    }
+
+    class CustomConfigureOlapParametersPageView : ConfigureOlapParametersPageView {
+        protected override OlapConnectionParametersControl CreateOlapConnectionParametersControl() {
+            return new CustomOlapConnectionParametersControl();
+        }
+    }
+
+    class CustomOlapConnectionParametersControl : OlapConnectionParametersControl {
+        public CustomOlapConnectionParametersControl() : base() {
+            lciConnectionType.Visibility = DevExpress.XtraLayout.Utils.LayoutVisibility.Never;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# WinForms Designer - How to customize the Data Source wizard for the OLAP data source
+
+
+This example shows how to remove the <strong>Connection type</strong> option from the Data Source wizard page that allows end-users to specify <a href="https://documentation.devexpress.com/#Dashboard/CustomDocument113908">OLAP connection parameters</a>. To do this, use the <a href="https://documentation.devexpress.com/#Dashboard/DevExpressDashboardWinDashboardDesigner_DataSourceWizardCustomizationServicetopic">DataSourceWizardCustomizationService</a> property.
+
+<br/>
+
+
